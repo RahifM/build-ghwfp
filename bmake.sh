c6152b224736788306ab0d660a1a7b2d92795027
RahifM <rahifmanjatha372@gmail.com>
2023-06-03 00:04:14 +0530

    track the change to commit

--- bmake.sh
+++ bmake.sh
@@ -10,7 +10,7 @@ notsame() {
 	git clone https://github.com/LineageOS/android_build -b lineage-18.1 ~/make-update
 	cd ~/make-update
 	git -C ~/make-update rev-parse --verify --short=8 HEAD 2>/dev/null > updatedcommitid
-	git commit -a -m "updatedcommitid"
+	git add . && git commit -a -m "updatedcommitid"
 	git remote add m https://github.com/RahifM/build
 	wget https://raw.githubusercontent.com/RahifM/repo_update/lineage-18.1-patches/relkey.patch && git apply relkey.patch
 	git commit -a -m "relkey"
