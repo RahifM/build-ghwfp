61a25383b76effa629eb0fe75c8874a85b752f91
RahifM <rahifmanjatha372@gmail.com>
2023-06-03 13:29:11 +0530

    try push again

--- bmake.sh
+++ bmake.sh
@@ -1,3 +1,5 @@
+export TZ='Asia/Kolkata' && date
+
 git clone https://github.com/LineageOS/android_build -b lineage-18.1 --depth 1 ~/make
 
 same() {
@@ -9,7 +11,8 @@ notsame() {
 	git clone https://github.com/LineageOS/android_build -b lineage-18.1 ~/make-update
 	git -C ~/make-update rev-parse --verify --short=8 HEAD 2>/dev/null > updatedcommitid
 	git add . && git commit -a -m "updatedcommitid $(date +'%Y%m%d-%H%M') [skip ci]"
-	git push origin HEAD
+	git remote add m https://github.com/RahifM/build-ghwfp
+	git push m HEAD
 	cd ~/make-update
 	git remote add m https://github.com/RahifM/build
 	wget https://raw.githubusercontent.com/RahifM/repo_update/lineage-18.1-patches/relkey.patch && git apply relkey.patch
